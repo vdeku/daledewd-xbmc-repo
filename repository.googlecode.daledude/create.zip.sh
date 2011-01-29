8a3b16ac823057f72a90c289b669d676e89bb317
daledewd <daledewd@gmail.com>
2011-01-29 21:25:53 +0000

    fixed addinfo.xml location in addon.xml.
    add zip, icon and helper script

---
+++ repository.googlecode.daledude/create.zip.sh
@@ -0,0 +1,4 @@
+cd ..
+rm repository.googlecode.daledude/repository.googlecode.daledude.zip
+zip repository.googlecode.daledude/repository.googlecode.daledude.zip repository.googlecode.daledude/addon.xml repository.googlecode.daledude/icon.png
+
